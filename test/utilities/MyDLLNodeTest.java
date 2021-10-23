684d42f4ba429f60088040836c81d38c9448590c
Sean <zhuqingyun@outlook.com>
2021-10-23 12:10:21 -0600

    setUp, tearDown MyDLLNodeTest update MyQueueTests

--- test/utilities/MyDLLNodeTest.java
+++ test/utilities/MyDLLNodeTest.java
@@ -7,32 +7,50 @@ public class MyDLLNodeTest<E> {
     private MyDLLNode myDLLNode;
     private MyDLLNode myDLLNode2;
 
+    /**
+     * @throws Exception if something wrong when assign and initialize MyArrayList
+     */
+
+    @Before
+    public void setUp() throws Exception
+    {
+        myDLLNode = new MyDLLNode("a");
+        myDLLNode2 = new MyDLLNode("b");
+    }
+
+    /**
+     * @throws Exception if clear method is failed
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        myDLLNode = null;
+        myDLLNode2 = null;
+    }
 
     @Test
     public void testGetElement() {
-        this.myDLLNode = new MyDLLNode("a");
+
         assertEquals("a", myDLLNode.getElement());
     }
 
     @Test
     public void testSetElement() {
-        this.myDLLNode = new MyDLLNode("a");
+
         this.myDLLNode.setElement("b");
         assertEquals("b", myDLLNode.getElement());
     }
 
     @Test
     public void testGetPrev() {
-        this.myDLLNode = new MyDLLNode("a");
-        this.myDLLNode2 = new MyDLLNode("b");
+
         this.myDLLNode.setPrev(myDLLNode2);
         assertEquals(myDLLNode2, myDLLNode.getPrev());
     }
 
     @Test
     public void testGetNext() {
-        this.myDLLNode = new MyDLLNode("a");
-        this.myDLLNode2 = new MyDLLNode("b");
+
         this.myDLLNode.setNext(myDLLNode2);
         assertEquals(myDLLNode2, myDLLNode.getNext());
     }
--- test/utilities/MyQueueTests.java
+++ test/utilities/MyQueueTests.java
@@ -1,32 +1,39 @@
 package utilities;
-
-
+import org.junit.Test;
+import utilities.MyQueue;
 import static org.junit.Assert.*;
-
 import org.junit.*;
 
-import utilities.MyQueue;
 
-public class MyQueueTests {
+import org.junit.After;
+import org.junit.Before;
 
+public class MyQueueTests<E> extends MyQueue<E> {
+	private MyQueue myQueue;
 
 	
 	/**
 	 * @throws Exception if something wrong when assign and initialize MyArrayList
 	 */
-	@Before
-	void setUp() throws Exception
-	{
 
+	@Before
+	public void setUp() throws Exception
+	{
+		myQueue = new MyQueue();
 	}
 	
 	/**
 	 * @throws Exception if clear method is failed
 	 */
 	@After
-	void tearDown() throws Exception
+	public void tearDown() throws Exception
 	{
-
+		myQueue.dequeueAll();
 	}
 
+	@Test
+	public void testCreate() {
+		super.create();
+		assertEquals(0, this.size());
+	}
 }
