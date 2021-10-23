7af555249d84f38da2bdfdd0f193213fee3c79c5
seanzhu <zhuqingyun@outlook.com>
2021-10-23 00:02:39 -0600

    Update MyDLLNodeTest testGetElement() testSetElement() testGetPrev() testGetNext()

--- test/utilities/MyDLLNodeTest.java
+++ test/utilities/MyDLLNodeTest.java
@@ -3,5 +3,37 @@ package utilities;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class MyDLLNodeTest {
+public class MyDLLNodeTest<E> {
+    private MyDLLNode myDLLNode;
+    private MyDLLNode myDLLNode2;
+
+
+    @Test
+    public void testGetElement() {
+        this.myDLLNode = new MyDLLNode("a");
+        assertEquals("a", myDLLNode.getElement());
+    }
+
+    @Test
+    public void testSetElement() {
+        this.myDLLNode = new MyDLLNode("a");
+        this.myDLLNode.setElement("b");
+        assertEquals("b", myDLLNode.getElement());
+    }
+
+    @Test
+    public void testGetPrev() {
+        this.myDLLNode = new MyDLLNode("a");
+        this.myDLLNode2 = new MyDLLNode("b");
+        this.myDLLNode.setPrev(myDLLNode2);
+        assertEquals(myDLLNode2, myDLLNode.getPrev());
+    }
+
+    @Test
+    public void testGetNext() {
+        this.myDLLNode = new MyDLLNode("a");
+        this.myDLLNode2 = new MyDLLNode("b");
+        this.myDLLNode.setNext(myDLLNode2);
+        assertEquals(myDLLNode2, myDLLNode.getNext());
+    }
 }
