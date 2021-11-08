53467fb2e8585f78fa084b87e3ab791cc04270cc
Cate Huang <s030228@gmail.com>
2021-11-08 15:53:24 -0700

    Added exception files

---
+++ src/exceptions/EmptyQueueException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class EmptyQueueException extends Exception {
+
+	public EmptyQueueException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyQueueException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
