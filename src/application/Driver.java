9db7855d174076939fc6b4e6b75c1cf07772b238
Cate Huang <s030228@gmail.com>
2021-11-10 18:48:00 -0700

    Added parser.

--- src/application/Driver.java
+++ src/application/Driver.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import exceptions.EmptyQueueException;
+import parser.Parser;
 import utilities.*;
 
 public class Driver {
 
-	public static <E> void main(String[] args) {
-
+	public static <E> void main(String[] args) throws FileNotFoundException, EmptyQueueException {
+		
+		if (args.length != 1)
+		{
+			System.out.println("Usage: When put (sample1.)xml file into res folder, the command will be:");
+			System.out.println("java -jar Parser.jar sample1.xml");
+		}
+		else
+		{
+			String xmlPath = "res/" + args[0];
+			File file = new File(xmlPath);
+			
+			if (! file.exists())
+			{
+				System.out.println("File not found.");
+			}
+			else
+			{
+				System.out.println("Parse for errors in the XML construction...");
+				Parser parser = new Parser(xmlPath);
+			}
+		}
+		
 	}
 }
---
+++ src/parser/Parser.java
@@ -0,0 +1,149 @@
+package parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.hamcrest.core.StringEndsWith;
+
+import exceptions.EmptyQueueException;
+import utilities.*;
+
+public class Parser {
+	
+	String xmlPath;
+	MyStack mystack = new MyStack();
+	MyQueue errorQ = new MyQueue();
+	MyQueue extrasQ = new MyQueue();
+	int count = 0;
+	
+	public Parser() {
+		super();
+	}
+
+	public Parser(String xmlPath) throws FileNotFoundException, EmptyQueueException 
+	{
+		String line;
+		String tag;
+		Scanner in = new Scanner(new File(xmlPath));
+		
+		while (in.hasNextLine())
+		{
+			line = in.nextLine().trim();
+
+			
+			if (line.startsWith("<?") && line.endsWith("?>"))
+			{
+				// ignore in this assignment
+			}
+			else if (line.startsWith("<") && line.endsWith("/>"))
+			{
+				// self-closing tag, no closing tags
+				// ignore
+			}
+			else if (line.startsWith("</") && line.endsWith(">"))
+			{
+				// closing tag, whose tag name matches to opening tag
+				tag = line.split("</")[1].split(">")[0];
+				//System.out.println("Tag: " + tag);
+				//System.out.println(getTagName(line) + " vs " + getTagName((String) mystack.peek()));
+				if (mystack.isEmpty())
+				{
+					errorQ.enqueue(line);
+				} 			
+				else if (getTagName(line).equals(getTagName((String) mystack.peek())))
+				{
+					mystack.pop();
+				}
+				else if (! errorQ.isEmpty() && (getTagName(line).equals(getTagName((String) errorQ.peek()))))
+				{
+					errorQ.dequeue();
+				}
+				else {
+					if (mystack.search(getTagName(line)) != -1)
+					{
+						while (! getTagName((String) mystack.peek()).equals(getTagName(line)))
+						{
+							count++;
+							System.out.println("Error: " + errorQ.enqueue(mystack.pop()));
+						}
+					}
+					else
+					{
+						extrasQ.enqueue(line);
+					}
+				}
+			}
+			else if (line.startsWith("<") && line.endsWith(">"))
+			{
+				// opening tag, which needs a closing tag
+				mystack.push(line);
+			}
+		}
+		
+		while (! mystack.isEmpty())
+		{
+			errorQ.enqueue(mystack.pop());
+		}
+		
+		if (errorQ.isEmpty() && !extrasQ.isEmpty())
+		{
+			while(! extrasQ.isEmpty())
+			{
+				count++;
+				System.out.println("Error: " + extrasQ.dequeue());
+			}
+		}		
+		if (! errorQ.isEmpty() && extrasQ.isEmpty())
+		{
+			while(! errorQ.isEmpty())
+			{
+				count++;
+				System.out.println("Error: " + errorQ.dequeue());
+			}
+		}
+		
+		while (! extrasQ.isEmpty() && ! errorQ.isEmpty())
+		{
+			if (! extrasQ.isEmpty() && ! extrasQ.isEmpty())
+			{
+				if (! extrasQ.peek().equals(errorQ.peek()))
+				{
+					count++;
+					System.out.println("Error: " + errorQ.dequeue());
+				}
+				else
+				{
+					extrasQ.dequeue();
+					errorQ.dequeue();
+				}
+			}
+		}
+		if (count == 0)
+		{
+			System.out.println("All good. Well constructed.");
+		}
+	}
+	
+	private String getTagName(String line) {
+		line = line.trim();
+		if (line.startsWith("</") && line.endsWith(">"))
+		{
+			return line.split("</")[1].split(">")[0];
+		}
+		else if (line.startsWith("<") && line.endsWith(">"))
+		{
+			if (line.contains(" "))
+			{
+				return line.split("<")[1].split(" ")[0];
+			}
+			else
+			{
+				return line.split("<")[1].split(">")[0];
+			}
+		}
+		return null;
+	}
+	
+
+}
